f5fbfe75c0373297abc4096d408241029dd1b034
Luiz Felipe Neves <Luiz.Lopes@thomsonreuters.com>
2024-11-21 16:48:23 -0300

    Next config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'soscar-lorena.vercel.app',
-            port: '',
-            pathname: '/**',
-          },
-          {
-            protocol: 'https',
-            hostname: 'images.unsplash.com',
-            port: '',
-            pathname: '/**',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'soscar-lorena.vercel.app',
+        port: '',
+        pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  // Configure SWC for browser environment
+  swcMinify: true,
+  experimental: {
+    forceSwcTransforms: true,
+  },
 };
 
 module.exports = nextConfig;
